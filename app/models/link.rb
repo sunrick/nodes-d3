c2e113071cdf1f8182f37b242be8c1d7bc6a1046
Rickard Sunden <rickard.sunden@outlook.com>
2016-11-15 20:13:52 -0500

    setup associations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,4 @@
 class Link < ApplicationRecord
+  belongs_to :source, foreign_key: :source_id, class_name: 'Node'
+  belongs_to :target, foreign_key: :target_id, class_name: 'Node'
 end
--- app/models/node.rb
+++ app/models/node.rb
@@ -1,2 +1,4 @@
 class Node < ApplicationRecord
+  has_many :sources, class_name: "Link", foreign_key: :source_id
+  has_many :targets, class_name: "Link", foreign_key: :target_id
 end
